f0027708542af4a7b9ac21757bfbbfde37cfe9c4
prashuna-shrestha <prashunashrestha01@gmail.com>
2026-01-16 21:45:11 +0545

    news page

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers import analysis, stocks, auth, technical_status
 from routers.predictions import router as lstm_predict
 from routers.market_movers import router as market_movers_router
+from routers.news import router as news_router
+
 
 app = FastAPI()
 
@@ -21,3 +23,4 @@ app.include_router(auth.router)
 app.include_router(technical_status.router, prefix="/api")
 app.include_router(lstm_predict, prefix="/api")
 app.include_router(market_movers_router, prefix="/api")
+app.include_router(news_router)  # no redirect_slashes parameter
